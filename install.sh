ad8bb2af060d3d45b87f8980b3c1fc7776f3393b
shadikur <me@shadikur.com>
2019-07-16 05:20:34 +0200

    Ready to use

--- install.sh
+++ install.sh
@@ -19,11 +19,22 @@ apt update -y && apt -y upgrade && apt install build-essential -y && apt install
 echo  "\n${bold}${green}System is ready for Proxmox Installation.${normal} \n"
 
 #Domain Name (Valid Hostname)
-echo "Please enter a valid hostname: "
+echo "${bold}Please enter a valid hostname: "
 read RESPONSE
 
 #Update Hostname
-echo "Hostname is updating ... \n"
+echo "Hostname is updating ... ${normal}\n"
 rm -rf /etc/hostname
-echo  "{RESPONSE}" >> /etc/hostname
+echo  ${RESPONSE} >> /etc/hostname
 echo "\n"
+echo "Enter the Public IP of this server"
+read PUBIP
+echo ${PUBIP} ${RESPONSE} >> /etc/hosts
+echo "${green}Please wait.. Proxmox is going to be installed very shortly ... ${normal}\n"
+echo "deb http://download.proxmox.com/debian/pve stretch pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
+wget http://download.proxmox.com/debian/proxmox-ve-release-5.x.gpg -O /etc/apt/trusted.gpg.d/proxmox-ve-release-5.x.gpg
+chmod +r /etc/apt/trusted.gpg.d/proxmox-ve-release-5.x.gpg  # optional, if you have a changed default umask
+apt update -y && apt dist-upgrade -y
+apt install proxmox-ve postfix open-iscsi -y
+echo "${green}Proxmox is ready to use. Please use the web browser for further configuration${normal}\n"
+reboot
